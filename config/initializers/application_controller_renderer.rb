2b3a9f5c44ece9d06a7472867586ab19cbf25873
tayboy217 <tayboy217@icloud.com>
2021-10-31 20:37:00 +0000

    Update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3c8013f05d3ad29ff05fbadb0704b56aa67428822d981fe804f9da914e5984d6d54e90cf0a415889030961b24def61a0dbc64229a24f4b6eef59ec5d07dca95c'
